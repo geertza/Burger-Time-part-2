75db07752701a8b500a12710150cf8c056d522a2
geertza <geertza@gmail.com>
2020-05-31 01:27:56 -0600

    all there time to tweak

---
+++ models/burger-model.js
@@ -0,0 +1,25 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // create a new burger in database
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  // update database value
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions to use in  controller
+module.exports = burger;
--- server-express.js
+++ server-express.js
@@ -1,19 +1,19 @@
 // import express module--------------------
 const express = require("express");
 const PORT = process.env.PORT || 8080;
+// Import routes and give the server access to them.
+var routes = require("./controllers/controller");
+
+
 app = express();
 
 // get(style/front js) content from the "public" directory.
 app.use(express.static("public"));
 
-// Set Handlebars.
+// Place Handlebars ino the steerer.
 const bars = require("express-handlebars");
 app.engine("handlebars", bars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-
-// Import routes and give the server access to them.
-var routes = require("./test");
 app.use(routes);
 // Start  server so that it can begin listening
 app.listen(PORT, function() {
