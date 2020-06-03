81bd0e3d527adc0174724e460ca8d984cb59f140
geertza <geertza@gmail.com>
2020-06-03 17:56:45 -0600

    final

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 var routes = require("./controllers/controller");
 var bodyParser= require("body-parser")
 
-app = express();
+const app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 
