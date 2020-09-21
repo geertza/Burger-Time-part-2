0a0b402385f1890516c872341c7bf9fca592756e
geertza <geertza@gmail.com>
2020-09-21 14:13:16 -0600

    heroku mtsql add

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql')
-var connection = mysql.createConnection({
+ 
+
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
     host: "localhost",
   //  port
     port: 3306,
@@ -9,7 +17,7 @@ var connection = mysql.createConnection({
     password: "password",
     database: "burgers"
   });
-
+}
   //  connection promise------------------------
   connection.connect(function(err) {
     if (err) throw err;
